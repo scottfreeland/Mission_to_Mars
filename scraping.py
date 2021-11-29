a58d4f6aef9007641739604b53ea1eed7bf8a642
Scott Freeland <90366840+scottfreeland@users.noreply.github.com>
2021-11-28 23:24:23 -0500

    Added Mission_to_Mars_Challenge code in both .ipynb and .py format, added index.html, and updated scraping.py and templates folder.

--- scraping.py
+++ scraping.py
@@ -19,7 +19,8 @@ def scrape_all():
         "news_paragraph": news_paragraph,
         "featured_image": featured_image(browser),
         "facts": mars_facts(),
-        "last_modified": dt.datetime.now()
+        "last_modified": dt.datetime.now(),
+        "hemispheres": hemispheres(browser)
     }
 
     # Stop webdriver and return data
@@ -96,6 +97,25 @@ def mars_facts():
 
     # Convert dataframe into HTML format, add bootstrap
     return df.to_html(classes="table table-striped")
+
+def hemispheres(browser):
+    url = 'https://marshemispheres.com/'
+    browser.visit(url)
+
+    hemisphere_image_urls = []
+
+    for i in range(4):
+        hemisphere = {}
+        browser.find_by_css("a.product-item h3")[i].click()
+        sample_img = browser.links.find_by_text("Sample").first
+        hemisphere["img_url"] = sample_img["href"]
+        hemisphere["title"] = browser.find_by_css("h2.title").text
+        hemisphere_image_urls.append(hemisphere)
+        browser.back()
+    
+    browser.quit()
+
+    return hemisphere_image_urls
 
 if __name__ == "__main__":
 
